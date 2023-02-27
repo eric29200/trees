057042f589800cb8f01d56d59a69301386f0a0b7
eric29200 <eric_begot@yahoo.fr>
2023-02-27 21:29:35 +0100

    AVL tree : implement tree_delete()

--- avl_tree.c
+++ avl_tree.c
@@ -15,6 +15,20 @@ static int node_height(struct node_t *node)
 }
 
 /*
+ * Find minimum value in a node.
+ */
+static struct node_t *node_min(struct node_t *node)
+{
+	if (!node)
+		return NULL;
+
+	if (!node->left)
+		return node;
+
+	return node_min(node->left);
+}
+
+/*
  * Right rotate subtree rooted with y.
  */
 struct node_t *right_rotate(struct node_t *y)
@@ -121,6 +135,88 @@ out:
 }
 
 /*
+ * Delete a value in a node.
+ */
+static struct node_t *node_delete(struct tree_t *tree, struct node_t *node, int val)
+{
+	struct node_t *tmp;
+	int balance;
+
+	if (!node)
+		return NULL;
+
+	/* delete in left child */
+	if (val < node->val) {
+		node->left = node_delete(tree, node->left, val);
+	/* delete in right child */
+	} else if (val > node->val) {
+		node->right = node_delete(tree, node->right, val);
+	/* this node must be deleted */
+	} else {
+		/* only one child or no child */
+		if (!node->left || !node->right) {
+			tmp = node->left ? node->left : node->right;
+
+			/* no child : just free this node */
+			if (!tmp) {
+				tmp = node;
+				node = NULL;
+				goto free_this_node;
+			}
+
+			/* one child : replace this node with it */
+			*node = *tmp;
+free_this_node:
+			/* free this node */
+			tree->size--;
+			free(tmp);
+			goto balance_this_node;
+		}
+
+		/* find minimum value in right child */
+		tmp = node_min(node->right);
+
+		/* set this node with minimum value */
+		node->val = tmp->val;
+
+		/* delete minimum value in right child */
+		node->right = node_delete(tree, node->right, node->val);
+	}
+
+balance_this_node:
+	if (!node)
+		return NULL;
+
+	/* update node height */
+	node->height = 1 + max(node_height(node->left), node_height(node->right));
+
+	/* compute node balance */
+	balance = node_balance(node);
+
+	/* left left case */
+	if (balance > 1 && node_balance(node->left) >= 0)
+		return right_rotate(node);
+
+	/* left right case */
+	if (balance > 1 && node_balance(node->left) < 0) {
+		node->left = left_rotate(node->left);
+		return right_rotate(node);
+	}
+
+	/* right right case */
+	if (balance < -1 && node_balance(node->right) <= 0)
+		return left_rotate(node);
+
+	/* right left case */
+	if (balance < -1 && node_balance(node->right) > 0) {
+		node->right = right_rotate(node->right);
+		return left_rotate(node);
+	}
+
+	return node;
+}
+
+/*
  * Insert a value in a tree.
  */
 static void tree_insert(struct tree_t *tree, int val)
@@ -129,6 +225,17 @@ static void tree_insert(struct tree_t *tree, int val)
 		return;
 
 	tree->root = node_insert(tree, tree->root, val);
+}
+
+/*
+ * Delete a value in a tree.
+ */
+static void tree_delete(struct tree_t *tree, int val)
+{
+	if (!tree)
+		return;
+
+	tree->root = node_delete(tree, tree->root, val);
 }
 
 /*
@@ -146,7 +253,7 @@ static void tree_balance(struct tree_t *tree)
 struct tree_operations_t avl_tree_ops = {
 	.height			= generic_tree_height,
 	.insert			= tree_insert,
-	.delete			= NULL,
+	.delete			= tree_delete,
 	.balance		= tree_balance,
 	.free			= generic_tree_free,
 };
